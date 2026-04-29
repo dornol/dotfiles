ed4e0a9c0f6957aee9f504db7d4f997ebbb6452b
DongHyeok Kim <dhkim@bookswage.com>
2026-04-29 13:01:55 +0900

    fix: 스니펫 소스 제거, 완성 수락 시 undo 브레이크포인트 추가

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,10 +1,27 @@
 return {
-  -- blink.cmp 멀티라인 ghost text 비활성화
   {
     "saghen/blink.cmp",
     opts = {
       completion = {
         ghost_text = { enabled = false },
+      },
+      -- 스니펫 소스 제거 (멀티라인 자동완성 방지)
+      sources = {
+        default = { "lsp", "path", "buffer" },
+      },
+      -- 완성 수락 시 undo 브레이크포인트 추가
+      keymap = {
+        ["<CR>"] = {
+          function(cmp)
+            if cmp.is_visible() then
+              vim.api.nvim_feedkeys(
+                vim.api.nvim_replace_termcodes("<C-g>u", true, true, true), "n", false
+              )
+              return cmp.accept()
+            end
+          end,
+          "fallback",
+        },
       },
     },
   },
