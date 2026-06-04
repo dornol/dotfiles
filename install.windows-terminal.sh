c2736dfc82df3df3aec6e03cb87e6aad0af31fe8
DongHyeok Kim <dhkim@bookswage.com>
2026-06-04 14:54:59 +0900

    rename: install-windows-terminal.* -> install.windows-terminal.*

--- install.windows-terminal.sh
+++ install.windows-terminal.sh
@@ -2,6 +2,6 @@
 set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PS1_WIN="$(wslpath -w "$SCRIPT_DIR/install-windows-terminal.ps1")"
+PS1_WIN="$(wslpath -w "$SCRIPT_DIR/install.windows-terminal.ps1")"
 
 powershell.exe -ExecutionPolicy Bypass -File "$PS1_WIN" "$@"
